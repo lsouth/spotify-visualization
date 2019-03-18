e49a1eb54cace95cbb45513c5e301efc93a46400
lsouth <lsouth269@gmail.com>
2019-03-18 17:31:51 -0400

    Add load functions

--- js/top-tracks-and-artists.js
+++ js/top-tracks-and-artists.js
@@ -1,1 +1,18 @@
-console.log("Top tracks and artists");
+function createTopTracks(tracks){
+  svg.append("g").attr("class","top-tracks");
+}
+
+function createTopArtists(artists){
+  svg.append("g").attr("class","top-artists");
+}
+
+function load(){
+  d3.json("data/laura-long-term-top-tracks.json", function(tracks){
+    createTopTracks(tracks);
+  });
+  d3.json("data/laura-short-term-top-artists.json", function(artists){
+    createTopArtists(artists);
+  });
+}
+
+load();
