161c820a52a4aaf7ee4d90ec6fa1954d6d43801e
Amogh Pradeep <amoghbl1@gmail.com>
2019-04-02 11:37:40 -0400

    Next is not what you think it is.

--- js/spotify-web-api.js
+++ js/spotify-web-api.js
@@ -1799,7 +1799,7 @@ function getRecentlyPlayed(swapi, number, lst = [], next = 0) {
         swapi.getMyRecentlyPlayedTracks(tobj).then(
             function(d){
                 console.log("Finished partial data fetch:", d);
-                var sn = d["next"].split("=")[1].split("&")[0];
+                var sn = d["cursor"]["after"];
                 getRecentlyPlayed(swapi, number - 50, lst.concat(d["items"]), sn);
             });
     } else {
